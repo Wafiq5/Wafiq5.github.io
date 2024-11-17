7d76c6a6e8e041dc4768a1122df10e903b3eef40
Wafiq <aswafiq8100@gmail.com>
2024-11-17 00:15:11 -0600

    Updated page

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.querySelector('.menu-toggle').addEventListener('click', function () {
     document.querySelector('.navbar-ul').classList.toggle('active');
     this.classList.toggle('active'); // Toggle the cross icon
 });
+console.log()

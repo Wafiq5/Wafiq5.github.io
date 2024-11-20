0e5ad81555949a6db051d800761aac0de011cc89
Wafiq <aswafiq8100@gmail.com>
2024-11-19 18:12:53 -0600

    Updated file structure for better organization + Started working on intersection observer.

--- script.js
+++ script.js
@@ -2,4 +2,3 @@ document.querySelector('.menu-toggle').addEventListener('click', function () {
     document.querySelector('.navbar-ul').classList.toggle('active');
     this.classList.toggle('active'); // Toggle the cross icon
 });
-console.log()

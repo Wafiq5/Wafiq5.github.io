f84a335003be3057b1c6726d832fa6c249a1bbb8
Wafiq <aswafiq8100@gmail.com>
2024-11-19 19:47:28 -0600

    Added Intersection Observer + Added animation when switching from each navbar + Bug fixes

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 document.querySelector('.menu-toggle').addEventListener('click', function () {
     document.querySelector('.navbar-ul').classList.toggle('active');
-    this.classList.toggle('active'); // Toggle the cross icon
+    this.classList.toggle('active');
 });
+
+// INTERSECTION OBSERVER FOR NAVBAR
+const navbar = document.querySelector(".navbar");
+const welcomeSection = document.querySelector(".welcome-section-container");
+
+const welcomeSectionOptions = { };
+
+const welcomeSectionObserver = new IntersectionObserver(function(entries, welcomeSectionObserver) {
+    entries.forEach(entry =>{
+        if (!entry.isIntersecting){
+            navbar.classList.add("bubble-navbar");
+            navbar.classList.remove("navbar");
+        }else{
+            navbar.classList.remove("bubble-navbar");
+            navbar.classList.add("navbar");
+        }
+    })
+}, welcomeSectionOptions);
+
+welcomeSectionObserver.observe(welcomeSection);
